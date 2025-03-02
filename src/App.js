d232f1467042222a9d9f618bd56e8a8079c4715e
Adeboss <ademolaadeigbe3@gmail.com>
2025-03-02 22:10:02 +0100

    UPDATED THE APP.JS

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends React.Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React TODAY 
           </a>
         </header>
       </div>
